9dd521de2beaa3123d0b4e6371e03ba76a4ca8d8
Michael Wilson <mw@ciphron.de>
2020-09-02 19:10:05 +0200

    simplify import structure

--- ansible_deployment/deployment.py
+++ ansible_deployment/deployment.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 from pprint import pformat
-from ansible_deployment.class_skeleton import AnsibleDeployment
-from ansible_deployment.role import Role
-from ansible_deployment.inventory import Inventory
-from ansible_deployment.playbook import Playbook
-from ansible_deployment.deployment_dir import DeploymentDirectory
+from ansible_deployment import (
+    AnsibleDeployment,
+    Role,
+    Inventory,
+    Playbook,
+    DeploymentDirectory)
 import json
 import subprocess
 
---
+++ ansible_deployment/inventory_plugins/__init__.py
@@ -0,0 +1,1 @@
+from ansible_deployment.inventory_plugins.terraform import Terraform
