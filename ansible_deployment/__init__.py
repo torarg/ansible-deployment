b7dff8f09b16fdb18e93775cd325dfb53b525d82
Michael Wilson <mw@ciphron.de>
2020-09-02 20:49:35 +0200

    use namedtuple DeploymentConfig for Deployment initialization

--- ansible_deployment/__init__.py
+++ ansible_deployment/__init__.py
@@ -4,5 +4,5 @@ from ansible_deployment.playbook import Playbook
 from ansible_deployment.inventory import Inventory
 from ansible_deployment.role import Role
 from ansible_deployment.inventory_plugins.terraform import Terraform
-from ansible_deployment.deployment import Deployment
+from ansible_deployment.deployment import DeploymentConfig, Deployment
 from ansible_deployment import cli_helpers
