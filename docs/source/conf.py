7142e4b337a5c447e1cdc6bf020e63f425169cab
Michael Wilson <mw@ciphron.de>
2020-09-07 01:32:04 +0200

    change sphinx theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,8 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage',
-              'sphinx.ext.napoleon', 'sphinx.ext.autosummary', 'sphinx_click']
+              'sphinx.ext.napoleon', 'sphinx.ext.autosummary', 'sphinx_click',
+              'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,7 +48,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'flask'
+#html_theme = 'flask'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
