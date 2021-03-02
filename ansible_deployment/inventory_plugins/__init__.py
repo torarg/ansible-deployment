07b0da738aca14aad2b23a1885adff26c810012a
Michael Wilson <mw@ciphron.de>
2021-03-02 02:46:31 +0100

    add vault plugin

--- ansible_deployment/inventory_plugins/__init__.py
+++ ansible_deployment/inventory_plugins/__init__.py
@@ -2,4 +2,5 @@
 ansible-deployment inventory plugins.
 """
 from ansible_deployment.inventory_plugins.terraform import Terraform
+from ansible_deployment.inventory_plugins.vault import Vault
 from ansible_deployment.inventory_plugins.inventory_plugin import InventoryPlugin
