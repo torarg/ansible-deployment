ffd7257ad1e27820a9a4f6d65298ea2dd0d5223c
Michael Wilson <mw@ciphron.de>
2021-03-03 23:08:01 +0100

    remove variable loading from inventory, since it is now handled as plugin

--- ansible_deployment/inventory.py
+++ ansible_deployment/inventory.py
@@ -44,13 +44,6 @@ class Inventory(AnsibleDeployment):
                 self._update_plugin_inventory(plugin)
                 self.loaded_plugins.append(plugin)
 
-            
-        self.vars = {
-            'host_vars': self.host_vars,
-            'group_vars': self.group_vars
-        }
-        self._load_vars('host_vars')
-        self._load_vars('group_vars')
         for plugin in self.loaded_plugins:
             plugin.update_inventory()
             self._update_plugin_inventory(plugin)
@@ -86,24 +79,6 @@ class Inventory(AnsibleDeployment):
         self.groups = self.plugin.groups
         self.host_vars = self.plugin.host_vars
         self.group_vars = self.plugin.group_vars
-
-    def _load_vars(self, vars_type):
-        """
-        Loads inventory variables from inventory path.
-
-        Args:
-            vars_type (str): Variable type. May be 'host_vars' or 'group_vars'.
-        """
-        ignore_patterns = ('.swp', )
-        vars_files = list((self.path / vars_type).glob('*'))
-        for vars_file in vars_files:
-            if any(map(vars_file.name.__contains__, ignore_patterns)):
-                continue
-            vars_name = vars_file.stem
-            with open(vars_file) as vars_file_stream:
-                self.vars[vars_type][vars_name] = yaml.safe_load(
-                    vars_file_stream)
-
 
     def write(self):
         """
