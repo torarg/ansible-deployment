a0877d1bc36cd1a3c7ce358ea4aa28f3763d0f88
Michael Wilson <mw@1wilson.org>
2021-07-31 01:15:08 +0200

    add ssh keypair support to vault inventory source

--- ansible_deployment/inventory_plugins/inventory_sources/vault.py
+++ ansible_deployment/inventory_plugins/inventory_sources/vault.py
@@ -14,50 +14,49 @@ class VaultReader(InventoryPlugin):
 
     name = "vault"
 
+    def read_secret(self, secret, fallback_value=None):
+        """
+        Reads a secret from vault.
+
+        secret (str): Vault secret path.
+        """
+        vault_path = f"ansible-deployment/{self.deployment_name}/{secret}"
+        try:
+            if secret == 'deployment_key':
+                query_result = vault_client.secrets.kv.read_secret_version(path=vault_path)[
+                    "data"
+                ]["data"]["data"]
+            else:
+                query_result = vault_client.secrets.kv.read_secret_version(path=vault_path)[
+                    "data"
+                ]["data"]
+        except vault_exceptions.InvalidPath:
+            query_result = fallback_value
+        return query_result
+        
     def update_inventory(self):
-        vault_base_path = f"ansible-deployment/{self.deployment_name}"
         vault_client, error = vault_helpers.init_vault_client()
 
         if error is not None:
             raise Exception(error)
 
-        vault_path = f"{vault_base_path}/hosts"
-        try:
-            query_result = vault_client.secrets.kv.read_secret_version(path=vault_path)[
-                "data"
-            ]["data"]
-        except vault_exceptions.InvalidPath:
-            query_result = dict()
-        self.hosts = query_result
-
-        vault_path = f"{vault_base_path}/deployment_key"
-        try:
-            query_result = vault_client.secrets.kv.read_secret_version(path=vault_path)[
-                "data"
-            ]["data"]["data"]
-            query_result = base64.decodebytes(query_result.encode("ascii"))
-        except vault_exceptions.InvalidPath:
-            query_result = None
-        self.deployment_key = query_result
+        self.hosts = self.read_secret("hosts", fallback_value=dict())
+        self.deployment_key = self.read_secret("deployment_key")
+        
+        self.ssh_keypair.private_key = self.read_secret("ssh_private_key")
+        self.ssh_keypair.public_key = self.read_secret("ssh_public_key")
+        self.ssh_keypair.private_key = self.read_secret("ssh_private_key_file")
+        self.ssh_keypair.public_key = self.read_secret("ssh_public_key_file")
 
         for group in self.groups + ["all"]:
-            vault_path = f"{vault_base_path}/group_vars/{group}"
-            try:
-                query_result = vault_client.secrets.kv.read_secret_version(
-                    path=vault_path
-                )["data"]["data"]
-            except vault_exceptions.InvalidPath:
-                query_result = dict()
-            self.group_vars[group] = query_result
+            secret_name = f"group_vars/{group}"
+            self.group_vars[group] = self.read_secret(secret=secret_name,
+                                                      fallback_value=dict())
 
         if "all" in self.hosts and "hosts" in self.hosts["all"]:
             for host in [*self.hosts["all"]["hosts"]]:
-                vault_path = f"{vault_base_path}/host_vars/{host}"
-                try:
-                    query_result = vault_client.secrets.kv.read_secret_version(
-                        path=vault_path
-                    )["data"]["data"]
-                except vault_exceptions.InvalidPath:
-                    query_result = dict()
-                self.host_vars[host] = query_result
-                self.all_hosts[host] = query_result
+                secret_name = f"host_vars/{host}"
+                host_vars = self.read_secret(secret=secret_name,
+                                             fallback_value=dict())
+                self.host_vars[host] = host_vars
+                self.all_hosts[host] = host_vars
