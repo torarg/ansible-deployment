48c725d7a2e2d89fd939afa9717c31903a67b112
Michael Wilson <mw@1wilson.org>
2022-01-03 14:33:07 +0100

    Fix collection module usage for python 3.10 and onward

--- ansible_deployment/inventory.py
+++ ansible_deployment/inventory.py
@@ -87,7 +87,7 @@ class Inventory(AnsibleDeployment):
         """
         for k, v in merge_dct.items():
             if (k in dct and isinstance(dct[k], dict)
-                    and isinstance(merge_dct[k], collections.Mapping)):
+                    and isinstance(merge_dct[k], collections.abc.Mapping)):
                 self._dict_merge(dct[k], merge_dct[k])
             else:
                 dct[k] = merge_dct[k]
