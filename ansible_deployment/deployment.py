dd37309f8a560aa436ddf07ac6824eb6e3132e62
mw <mw@1wilson.org>
2022-12-08 11:30:49 +0100

    use namedtuple DeploymentConfig for Deployment initialization

--- ansible_deployment/deployment.py
+++ ansible_deployment/deployment.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from pprint import pformat
+from collections import namedtuple
 from ansible_deployment import (
     AnsibleDeployment,
     Role,
@@ -10,14 +11,30 @@ import json
 import subprocess
 
 
+DeploymentConfig = namedtuple('DeploymentConfig', 'roles roles_src inventory_plugin')
 class Deployment(AnsibleDeployment):
     filtered_values = ['playbook', 'inventory']
+    def load(deployment_state_file):
+        deployment = None
+        deployment_state_file_path = Path(deployment_state_file)
+        deployment_path = deployment_state_file_path.parent
+        deployment_config = Deployment._load_state_file(deployment_state_file_path)
+        deployment = Deployment(deployment_path, deployment_config)
+        return deployment
 
-    def __init__(self, deployment_path, roles_src, roles, inventory_plugin):
-        self.deployment_dir = DeploymentDirectory(deployment_path, roles_src)
+    def _load_state_file(state_file_path):
+        with open(state_file_path) as state_file_stream:
+            deployment_state = json.load(state_file_stream)
+        deployment_path = state_file_path.parent
+
+        return DeploymentConfig(**deployment_state)
+
+    def __init__(self, path, config):
+        self.deployment_dir = DeploymentDirectory(path, config.roles_src)
         self.name = self.deployment_dir.path.name
-        self.roles = self._create_role_objects(roles)
-        self.inventory = Inventory(self.deployment_dir.path, inventory_plugin)
+        self.config = config
+        self.roles = self._create_role_objects(config.roles)
+        self.inventory = Inventory(self.deployment_dir.path, config.inventory_plugin)
         self.playbook = Playbook(self.deployment_dir.path / 'playbook.yml',
                                  'all', self.roles)
 
@@ -27,6 +44,7 @@ class Deployment(AnsibleDeployment):
             parsed_roles.append(
                 Role(self.deployment_dir.roles_path / role_name))
         return parsed_roles
+
 
     def initialize_deployment_directory(self):
         role_names = (role.name for role in self.roles)
@@ -38,18 +56,8 @@ class Deployment(AnsibleDeployment):
         self.deployment_dir.update_git(message="add deployment files")
 
     def save(self):
-        role_names = []
-        for role in self.roles:
-            role_names.append(role.name)
-
-        deployment_state = {
-            'name': self.name,
-            'roles': role_names,
-            'inventory_plugin': self.inventory.plugin.name,
-            'ansible_roles_src': self.deployment_dir.roles_src
-        }
         with open(self.deployment_dir.state_file, 'w') as state_file_stream:
-            json.dump(deployment_state, state_file_stream, indent=4)
+            json.dump(self.config._asdict(), state_file_stream, indent=4)
 
     def run(self, tags=None):
         command = ['ansible-playbook', 'playbook.yml']
@@ -71,18 +79,4 @@ class Deployment(AnsibleDeployment):
     def update(self):
         self.deployment_dir.update(self.roles, self.playbook, self.inventory)
 
-    def load(deployment_state_file):
-        deployment = None
-        deployment_state_file_path = Path(deployment_state_file)
-        if deployment_state_file_path.exists():
-            with open(deployment_state_file_path
-                      ) as deployment_state_file_stream:
-                deployment_state = json.load(deployment_state_file_stream)
-                deployment_path = deployment_state_file_path.parent
 
-            deployment = Deployment(deployment_path,
-                                    deployment_state['ansible_roles_src'],
-                                    deployment_state['roles'],
-                                    deployment_state['inventory_plugin'])
-
-        return deployment
