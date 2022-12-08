021526aa700f6eb0bea17a5c48848a38e0f16ada
mw <mw@1wilson.org>
2022-12-08 11:29:50 +0100

    bump docs version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,11 +18,11 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'ansible_deployment'
-copyright = '2020, Michael Wilson'
+copyright = '2022, Michael Wilson'
 author = 'Michael Wilson'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
