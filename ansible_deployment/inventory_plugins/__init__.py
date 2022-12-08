67fa0027c46fe291ce14ed006863d5604c051c1d
mw <mw@1wilson.org>
2022-12-08 11:30:53 +0100

    add local inventory plugin

--- ansible_deployment/inventory_plugins/__init__.py
+++ ansible_deployment/inventory_plugins/__init__.py
@@ -3,4 +3,5 @@ ansible-deployment inventory plugins.
 """
 from ansible_deployment.inventory_plugins.terraform import Terraform
 from ansible_deployment.inventory_plugins.vault import Vault
+from ansible_deployment.inventory_plugins.local import Local
 from ansible_deployment.inventory_plugins.inventory_plugin import InventoryPlugin
---
+++ ansible_deployment/inventory_plugins/local.py
@@ -0,0 +1,41 @@
+import yaml
+from pathlib import Path
+from ansible_deployment.inventory_plugins.inventory_plugin import (
+    InventoryPlugin)
+
+class Local(InventoryPlugin):
+    """
+    Local InventoryPlugin class.
+    """
+
+    def _load_vars(self, vars_type):
+        """
+        Loads inventory variables from inventory path.
+
+        Args:
+            vars_type (str): Variable type. May be 'host_vars' or 'group_vars'.
+        """
+        vars_dir_path = Path(f"./{vars_type}")
+        ignore_patterns = ('.swp', )
+        vars_files = list(vars_dir_path.glob('*'))
+        for vars_file in vars_files:
+            if any(map(vars_file.name.__contains__, ignore_patterns)):
+                continue
+            vars_name = vars_file.stem
+            with open(vars_file) as vars_file_stream:
+                self.vars[vars_type][vars_name] = yaml.safe_load(
+                    vars_file_stream)
+
+    def _load_hosts(self):
+        hosts_file_path = Path("./hosts.yml")
+        if hosts_file_path.exists():
+            with open(hosts_file_path) as f:
+                self.hosts = yaml.safe_load(f)
+
+    def update_inventory(self):
+        self.hosts = {}
+        self.host_vars = {}
+        
+        self._load_hosts()
+        self._load_vars('host_vars')
+        self._load_vars('group_vars')
