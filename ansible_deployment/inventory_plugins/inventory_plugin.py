7fd315e7a0e963122efabda649a9bf01408df8b0
mw <mw@1wilson.org>
2022-12-08 11:30:56 +0100

    add group variable for referencing ssh public key

--- ansible_deployment/inventory_plugins/inventory_plugin.py
+++ ansible_deployment/inventory_plugins/inventory_plugin.py
@@ -36,7 +36,8 @@ class InventoryPlugin:
         self.host_vars = {}
         self.group_vars = {}
         self.group_vars["all"] = {"ansible_user": "ansible",
-                                  "ansible_ssh_private_key_file": "./.ssh/id_rsa"}
+                                  "ansible_ssh_private_key_file": "./.ssh/id_rsa",
+                                  "ansible_ssh_public_key_file": "./.ssh/id_rsa.pub"}
         self._set_groups()
         self.added_files = []
         self.vars = {"host_vars": self.host_vars, "group_vars": self.group_vars}
