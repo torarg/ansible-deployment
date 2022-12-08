c49503aba04afdbaa91eecc14a4296f6157959ad
mw <mw@1wilson.org>
2022-12-08 11:30:53 +0100

    split up inventory plugins into readers and writers

--- ansible_deployment/inventory_plugins/__init__.py
+++ ansible_deployment/inventory_plugins/__init__.py
@@ -2,6 +2,8 @@
 ansible-deployment inventory plugins.
 """
 from ansible_deployment.inventory_plugins.terraform import Terraform
+from ansible_deployment.inventory_plugins.vault_reader import VaultReader
+from ansible_deployment.inventory_plugins.vault_writer import VaultWriter
 from ansible_deployment.inventory_plugins.vault import Vault
 from ansible_deployment.inventory_plugins.local import Local
 from ansible_deployment.inventory_plugins.inventory_plugin import InventoryPlugin
